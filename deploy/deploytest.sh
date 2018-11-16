6ec21a3015fa3aa1e84c4c05218d44413e972f1e
Fernando Matos <prudente.matos@hotmail.com>
2018-11-16 16:09:38 -0200

    adicionando validação de falha de deploy travis

--- deploy/deploytest.sh
+++ deploy/deploytest.sh
@@ -1,9 +1,9 @@
 docker run --rm fpmatos/nodetesteds echo "teste de container OK"
 
 if [ ! $? -eq 0 ]; then
- echo "Erro durante o teste de deploy" >> deploy_log.txt
- travis_terminate 1;
+ echo "Erro durante o teste de deploy" >> deploy_log.txt 
  exit 125
 else
  echo "Deploy de container docker testado e está OK" >> deploy_log.txt
+ exit 0
 fi;
